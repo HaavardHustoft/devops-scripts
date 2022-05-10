92327b1447b6d9b7781d51a2169bf25befe6dd01
HaavardHustoft <40405151+HaavardHustoft@users.noreply.github.com>
2022-05-10 16:58:01 +0200

    Update edit-conf.sh

--- kubernetes/edit-conf.sh
+++ kubernetes/edit-conf.sh
@@ -1,16 +1,16 @@
 #!bin/bash
-#Usage: sudo sh edit-conf ip
+ip=$(hostname -I | head -n1 | cut -d " " -f1)
 cert=$(sudo cat /etc/kubernetes/admin.conf | grep "certificate-authority")
 server=$(sudo cat /etc/kubernetes/admin.conf | grep "server")
 
 sudo sed -i "4s/.*/$cert/" /home/tim/.kube/config
-sudo sed -i "5s/.*/    server: https:\/\/$1:6443/" /home/tim/.kube/config
+sudo sed -i "5s/.*/    server: https:\/\/$ip:6443/" /home/tim/.kube/config
 
 sudo sed -i "4s/.*/$cert/" /home/janet/.kube/config
-sudo sed -i "5s/.*/    server: https:\/\/$1:6443/" /home/janet/.kube/config
+sudo sed -i "5s/.*/    server: https:\/\/$ip:6443/" /home/janet/.kube/config
 
 sudo sed -i "4s/.*/$cert/" /home/alice/.kube/config
-sudo sed -i "5s/.*/    server: https:\/\/$1:6443/" /home/alice/.kube/config
+sudo sed -i "5s/.*/    server: https:\/\/$ip:6443/" /home/alice/.kube/config
 
 sudo mkdir -p /home/ubuntu/rolebindings
 wget -O /home/ubuntu/rolebindings/janet-rolebinding.yaml https://raw.githubusercontent.com/HaavardHustoft/devops-scripts/main/kubernetes/alice-role-binding.yaml
