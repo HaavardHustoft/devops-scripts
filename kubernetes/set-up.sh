1a8fab8cd5830edb9a3e43833494d97db2b41d8e
HaavardHustoft <40405151+HaavardHustoft@users.noreply.github.com>
2022-05-10 16:50:01 +0200

    Update set-up.sh

--- kubernetes/set-up.sh
+++ kubernetes/set-up.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 
+name=$(hostname)
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
@@ -9,6 +10,6 @@ helm repo update
 
 sudo kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.22/deploy/local-path-storage.yaml
 sudo kubectl patch storageclass local-path -n jupyter -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
-sudo kubectl taint nodes jupyter node-role.kubernetes.io/control-plane-
-sudo kubectl taint nodes jupyter node-role.kubernetes.io/master-
+sudo kubectl taint nodes $name node-role.kubernetes.io/control-plane-
+sudo kubectl taint nodes $name node-role.kubernetes.io/master-
 sudo helm upgrade --cleanup-on-fail   --install jupyterhub jupyterhub/jupyterhub   --namespace jupyter   --create-namespace   --version=1.2.0   --values config.yaml
