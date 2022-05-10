a20ccb9a53810f55e8f5bda87223eb5975c12705
Håvard Hustoft <haavard@favrit.com>
2022-05-10 17:14:20 +0200

    move to folder

--- kubernetes/edit-conf.sh
+++ kubernetes/edit-conf.sh
@@ -12,7 +12,4 @@ sudo sed -i "5s/.*/    server: https:\/\/$ip:6443/" /home/janet/.kube/config
 sudo sed -i "4s/.*/$cert/" /home/alice/.kube/config
 sudo sed -i "5s/.*/    server: https:\/\/$ip:6443/" /home/alice/.kube/config
 
-sudo mkdir -p /home/ubuntu/rolebindings
-wget -O /home/ubuntu/rolebindings/janet-rolebinding.yaml https://raw.githubusercontent.com/HaavardHustoft/devops-scripts/main/kubernetes/alice-role-binding.yaml
-wget -O /home/ubuntu/rolebindings/tim-rolebinding.yaml https://raw.githubusercontent.com/HaavardHustoft/devops-scripts/main/kubernetes/tim-role-binding.yaml
-wget -O /home/ubuntu/rolebindings/alice-rolebinding.yaml https://raw.githubusercontent.com/HaavardHustoft/devops-scripts/main/kubernetes/alice-role-binding.yaml
+sudo kubectl apply -f devops-scripts/kubernetes/rolebindings
